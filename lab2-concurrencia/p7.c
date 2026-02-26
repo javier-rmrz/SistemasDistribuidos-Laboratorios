b4a18a3d6ca7196924d1d9bfd7b2067c46dce7d4
100474857 <100474857@alumnos.uc3m.es>
2026-02-26 11:25:42 +0100

    p7 (ej8) del lab2-concurrencia

--- lab2-concurrencia/p7.c
+++ lab2-concurrencia/p7.c
@@ -9,67 +9,73 @@
 #define NUM_THREADS	11    // 10 lectores y 1 escritor
 #define ITER 		40
 
-pthread_mutex_t	mutex_recurso;
-pthread_mutex_t	mutex_lectores;
-int		num_lectores;
+pthread_mutex_t mtx;
+pthread_cond_t can_read;
+pthread_cond_t can_write;
+
+int active_readers = 0;
+int active_writer = 0;   // 0/1
 int		recurso = 0;
 
-void lector(void) {
+void *lector(void *arg) {
 	int j;
-	int s;
+	unsigned int s = (unsigned int)time(NULL) ^ (unsigned int)(uintptr_t)pthread_self();
 	double k;
 
 	for(j=0 ; j < ITER; j++) {
 		k = (double) rand_r((unsigned int *) &s) / RAND_MAX;	
 		usleep((int) 1+(k * 6000000)); // duerme entre 1 y 9 s
 
-		if (pthread_mutex_lock(&mutex_lectores)!=0)
-			printf(".......  Error en mutex lock 1 \n");
-		num_lectores++;
-
-		if (num_lectores == 1) {
-			printf("Primer %lu \n", (unsigned long int) pthread_self());
-			pthread_mutex_lock(&mutex_recurso);
+		if (pthread_mutex_lock(&mtx) != 0)
+			printf(".......  Error en mutex lock (lector) \n");
+		while (active_writer) {
+			pthread_cond_wait(&can_read, &mtx);
 		}
-
-		if (pthread_mutex_unlock(&mutex_lectores)!=0)
-			printf(".......  Error en mutex unlock 1 \n");
+		active_readers++;
+		if (pthread_mutex_unlock(&mtx) != 0)
+			printf(".......  Error en mutex unlock (lector) \n");
 
 		printf("       Ejecuta el lector %lu lee  %d \n", (unsigned long int) pthread_self(), recurso );
 
-		if (pthread_mutex_lock(&mutex_lectores)!=0)
-			printf(".......  Error en mutex lock 2 \n");
-		num_lectores--;
-
-		if (num_lectores == 0){
-			printf("Ultimo %lu \n", (unsigned long int) pthread_self());
-			if( pthread_mutex_unlock(&mutex_recurso) !=0)
-				printf("________________ Error en unlock\n");
+		if (pthread_mutex_lock(&mtx) != 0)
+			printf(".......  Error en mutex lock (lector fin) \n");
+		active_readers--;
+		if (active_readers == 0) {
+			pthread_cond_signal(&can_write);
 		}
-
-		if (pthread_mutex_unlock(&mutex_lectores)!=0)
-			printf(".......  Error en mutex unlock 2 \n");
+		if (pthread_mutex_unlock(&mtx) != 0)
+			printf(".......  Error en mutex unlock (lector fin) \n");
 	}
 	
 	pthread_exit(NULL);
 
 }
 
-void escritor(void) {
+void *escritor(void *arg) {
 	int j;
-	int s;
+	unsigned int s = (unsigned int)time(NULL) ^ (unsigned int)(uintptr_t)pthread_self() ^ 0x9e3779b9u;
 	double k;
 
 	for(j=0 ; j < ITER; j++) {
 		k = (double) rand_r((unsigned int *) &s) / RAND_MAX;	
 		usleep((int) 2 + (k * 5000000)); // duerme entre 2 y 7 s
 
-		pthread_mutex_lock(&mutex_recurso);
+		pthread_mutex_lock(&mtx);
+		while (active_writer || active_readers > 0) {
+			pthread_cond_wait(&can_write, &mtx);
+		}
+		active_writer = 1;
+		pthread_mutex_unlock(&mtx);
 
 		recurso = recurso + 1;
 		printf("                                                          Escritor: Nuevo valor %d\n", recurso);
 
-		pthread_mutex_unlock(&mutex_recurso);
+		pthread_mutex_lock(&mtx);
+		active_writer = 0;
+		// Prioridad a lectores: despertar a todos los lectores primero
+		pthread_cond_broadcast(&can_read);
+		pthread_cond_signal(&can_write);
+		pthread_mutex_unlock(&mtx);
 	
 	}
 	
@@ -83,27 +89,23 @@ int main(int argc, char *argv[])
 	pthread_attr_t attr;
 	pthread_t thid[NUM_THREADS];
 	struct timeval t;
-	pthread_mutexattr_t   mattr;
 
 	gettimeofday(&t, NULL);
 	srand(t.tv_sec);	// se inicializa la semilla de nª pseudoaleatorios
 
 	pthread_attr_init(&attr);
 
-	pthread_mutexattr_init(&mattr);
-	pthread_mutexattr_settype(&mattr, PTHREAD_MUTEX_ERRORCHECK);
-
-
-	pthread_mutex_init(&mutex_recurso, &mattr);
-	pthread_mutex_init(&mutex_lectores, &mattr);
+	pthread_mutex_init(&mtx, NULL);
+	pthread_cond_init(&can_read, NULL);
+	pthread_cond_init(&can_write, NULL);
 
 
 	for (j = 0; j < NUM_LECTORES; j++)
-		if (pthread_create(&thid[j], NULL, (void *) lector, NULL) == -1){
+		if (pthread_create(&thid[j], NULL, lector, NULL) != 0){
 			printf("Error al crear los threads lectores\n");
 			exit(0);
 		}
-	if (pthread_create(&thid[j], NULL, (void *) escritor, NULL) == -1){
+	if (pthread_create(&thid[j], NULL, escritor, NULL) != 0){
 		printf("Error al crear el escritor\n");
 		exit(0);
 	}
@@ -116,5 +118,6 @@ int main(int argc, char *argv[])
 }
 	
 
-	
-
+// PROBLEMA: El primer lector bloquea el mutex, pero el último lector es quien lo desbloquea, lo cual no se puede con 
+// 			 un mutex dado que debe desbloquearlo el mismo lector que lo bloqueó. Esto provoca que el mutex_recurso se
+// 			 se quede bloqueado permanentemente, mientras que el escritor se queda atascado en él.
